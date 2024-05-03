eda1760d89f0ddca4e9fc08f4c964c176291cbf5
Lis3ee <godard.lise@gmail.com>
2024-05-03 18:15:43 +0200

    Commit page home and services

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/services", "Nos services", "/pages/services.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
